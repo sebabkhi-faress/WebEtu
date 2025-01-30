fbc355f7873806d7127ee282c68aaa32ee8192c5
batoutiva <benramoulasma@gmail.com>
2025-01-30 19:35:35 +0100

    New About page UI

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,130 +4,107 @@ export const metadata = {
   title: "WebEtu - About",
 }
 
-const contentStyle =
-  "text-md sm:text-xl leading-loose sm:leading-relaxed text-gray-700 sm:mr-4"
-
-const Section = ({ title, children, titleColor }: any) => {
-  return (
-    <section className="mb-10">
-      <h2 className={`text-xl sm:text-2xl font-semibold sm:m-2 ${titleColor}`}>
-        {title}
-      </h2>
-      {children}
-    </section>
-  )
-}
-
-const List = ({ items }: any) => {
-  return (
-    <ul className={`${contentStyle} list-disc list-inside space-y-2`}>
-      {items.map((item: any, index: any) => (
-        <li key={index} className="pr-4">
-          {item}
-        </li>
-      ))}
-    </ul>
-  )
-}
-
-// AboutPage Component
 const AboutPage = () => {
   return (
-    <article
-      className="rtl p-6 sm:p-10 max-w-6xl mx-auto font-sans text-gray-800"
-      dir="rtl"
-    >
-      <header className="text-center mb-12">
-        <div className="flex justify-center items-center mt-6 px-4">
+    <div className="min-h-screen w-screen flex items-center justify-center bg-gray-100 p-8 pt-20 sm:pt-8">
+      <div className="max-w-7xl w-full bg-white shadow-xl rounded-2xl p-12 text-gray-800 font-sans grid grid-cols-1 sm:grid-cols-2 gap-12 h-auto">
+        <div className="flex flex-col justify-center items-center text-center">
           <Image
             priority={true}
             src="/images/logo-black.svg"
             alt="OSCA Club Logo"
-            width={150}
-            height={150}
+            width={170}
+            height={170}
+            className="mb-12 rounded-full shadow-lg"
           />
+          <h1 className="text-4xl font-bold text-gray-900 mb-4">
+            مرحبًا بكم في WebEtu
+          </h1>
+          <p className="text-2xl text-gray-600 font-semibold">
+            جامعة باجي مختار
+          </p>
+          <div className="mt-8 flex gap-6 justify-center sm:justify-start">
+            <a
+              href="https://t.me/OSCommunityChat"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sky-600 hover:text-sky-400 transition"
+            >
+              <i className="fab fa-telegram-plane text-5xl"></i>
+            </a>
+            <a
+              href="https://github.com/OSCAnnaba/WebEtu"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-800 hover:text-gray-600 transition"
+            >
+              <i className="fab fa-github text-5xl"></i>
+            </a>
+          </div>
+          <h2 className="text-2xl font-semibold text-sky-700 mt-12">
+            فريق العمل
+          </h2>
+          <ul className="list-inside space-y-3 text-lg mt-4 pl-0">
+            <li>قصاص أحمد عبد النور</li>
+            <li>شدادي خليل عبد الرحمان</li>
+          </ul>
         </div>
-        <h1 className="text-2xl sm:text-3xl font-bold text-gray-900 mt-4">
-          مرحبًا بكم في WebEtu
-        </h1>
-        <p className="text-md sm:text-xl text-gray-600 mt-2 font-semibold">
-          جامعة باجي مختار
-        </p>
-      </header>
 
-      <Section title="من نحن؟" titleColor="text-green-700">
-        <p className={contentStyle}>
-          نحن نادي علمي يتكون من مجموعة من طلاب الإعلام الآلي في جامعة باجي
-          مختار بولاية عنابة، يطلق على نادينا إسم مجتمع البرمجيات مفتوحة المصدر،
-          حيث نسعى لتعزيز التعاون وتبادل المعرفة في مجال البرمجيات الحرة.
-        </p>
-      </Section>
+        <div className="grid grid-cols-1 gap-8 text-right">
+          <Section title="من نحن؟" titleColor="text-green-700">
+            <p>
+              نحن نادي علمي يضم مجموعة من طلاب الإعلام الآلي بجامعة باجي مختار،
+              نسعى لتعزيز التعاون وتبادل المعرفة في مجال البرمجيات الحرة
+            </p>
+          </Section>
 
-      <Section title="ما هو هذا الموقع؟" titleColor="text-green-700">
-        <p className={contentStyle}>
-          هذا الموقع هو نسخة ويب بديلة غير رسمية من تطبيق{" "}
-          <a
-            href="https://play.google.com/store/apps/details?id=app.progres.webetu&hl=en"
-            className="font-bold underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            WebEtu
-          </a>
-        </p>
-      </Section>
+          <Section title="ما هو هذا الموقع؟" titleColor="text-green-700">
+            <p>
+              هذا الموقع هو نسخة ويب بديلة غير رسمية من تطبيق{" "}
+              <a
+                href="https://play.google.com/store/apps/details?id=app.progres.webetu&hl=en"
+                className="font-bold underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                WebEtu
+              </a>
+            </p>
+          </Section>
 
-      <Section title="ما سبب تطوير هذا الموقع؟" titleColor="text-green-700">
-        <p className={contentStyle}>هناك سببان رئيسيان يتمثلان في:</p>
-        <List
-          items={[
-            "دعم مختلف المنصات الإلكترونية سواء أجهزة حاسوب أو الهواتف أو أي جهاز رقمي أخر به متصفح",
-            "بعض المشاكل التقنية الموجودة في التطبيق الرسمي الخاص بوزارة التعليم العالي والبحث العلمي",
-          ]}
-        />
-      </Section>
+          <Section title="ما سبب تطوير هذا الموقع؟" titleColor="text-green-700">
+            <p className="text-lg text-gray-700 font-medium">
+              هناك سببان رئيسيان يتمثلان في:
+            </p>
+            <List
+              items={[
+                "دعم مختلف المنصات الإلكترونية سواء أجهزة حاسوب أو الهواتف أو أي جهاز رقمي أخر به متصفح",
+                "بعض المشاكل التقنية الموجودة في التطبيق الرسمي الخاص بوزارة التعليم العالي والبحث العلمي",
+              ]}
+            />
+          </Section>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-      <Section title="الإبلاغ عن مشكلة" titleColor="text-sky-700">
-        <p className={contentStyle}>
-          في حال أردت التبليغ عن مشكلة أو تقديم إقتراح لتحسين الموقع، يُرجى
-          الإنضمام إلى مجموعتنا في{" "}
-          <a
-            href="https://t.me/OSCommunityChat"
-            className="font-bold underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            التيليجرام
-          </a>
-          .
-        </p>
-      </Section>
-      <Section title="فريق العمل" titleColor="text-sky-700">
-        <List items={["قصاص أحمد عبد النور", "شدادي خليل عبد الرحمان"]} />
-      </Section>
+const Section = ({ title, children, titleColor }: any) => {
+  return (
+    <div className="p-6 rounded-lg bg-gray-50 shadow-md border border-gray-200">
+      <h2 className={`text-2xl font-semibold mb-4 ${titleColor}`}>{title}</h2>
+      <div className="text-gray-700 text-lg">{children}</div>
+    </div>
+  )
+}
 
-      <footer className="text-center mt-12">
-        <a
-          href="https://t.me/OSCommunityChat"
-          className="text-sky-600/80 hover:text-sky-500 transition duration-200"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fab fa-telegram-plane text-4xl sm:text-5xl"></i>{" "}
-        </a>
-        <a
-          href="https://github.com/OSCAnnaba/WebEtu"
-          className="text-gray-800 hover:text-gray-600 transition duration-200 ml-4"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fab fa-github text-4xl sm:text-5xl"></i>
-        </a>
-        <p className="text-gray-500 text-sm sm:text-md mt-4">
-          🄯 2024 OSCA - All Lefts Unreserved
-        </p>
-      </footer>
-    </article>
+const List = ({ items }: { items: string[] }) => {
+  return (
+    <ul className="list-inside space-y-3 rtl text-gray-700">
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
   )
 }
 
